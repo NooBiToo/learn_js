befb9d855a936f2a9b2fd62c5ac5887ea4c46004
Эрл Капаров <mr.kaparov@gmail.com>
2021-10-27 22:33:39 +0800

    git init

---
+++ example_1/common.js
@@ -0,0 +1,51 @@
+//comment
+//BABEL
+/*
+1) number
+2) bigint
+3) string
+4) boolean: true/false
+5) null
+6) undefined
+7) object {}
+8) symbol 
+
+
+= - присваевание
+== - сопоставление не строгое 10 == '10' /true
+=== - сопоставление строгое 10 === '10' /false
+*/
+const COLOR_RED = "#F00"
+let alertFunction 
+
+
+const a = {
+  "a": '123'
+  [
+    addEventListener, asdas
+  ]
+}
+let b = 10
+window.getSelection('test_render')
+let c
+b = null
+array = ['a', 'b']
+toSting(array) = 'a,b'
+Number("124") = 124
+Number()
+
+{
+  "usernick" = "Jonh",
+  "old" = 20
+  'interes' = [
+    1,2,3
+  ]
+}
+
+
+if(b === null)
+
+
+console.log('переменная a ' + typeof(a))
+console.log('переменная с ' + typeof(с))
+console.log(typeof(b))
---
+++ example_2/common.js
@@ -0,0 +1,8 @@
+// let question = prompt('Это понятно?')
+// if (question == 'да') {
+//   document.querySelector(".result").innerHTML = "Замечательно";
+// } else if (question == 'нет') {
+//   document.querySelector(".result").innerHTML = "Надо бы повторить";
+// } else {
+//   document.querySelector(".result").innerHTML = "Ответ не ясен";
+// }
