7ccc50ee3bc03cbab04d81370f7653ccfe91c3e4
Эрл Капаров <mr.kaparov@gmail.com>
2021-10-28 22:05:17 +0800

    UPD

--- example_2/common.js
+++ example_2/common.js
@@ -1,8 +1,39 @@
 // let question = prompt('Это понятно?')
 // if (question == 'да') {
-//   document.querySelector(".result").innerHTML = "Замечательно";
-// } else if (question == 'нет') {
-//   document.querySelector(".result").innerHTML = "Надо бы повторить";
+//   document.querySelector('.conditional__result').innerHTML = 'Замечательно'
+// } else if (question === 'нет') {
+//   document.querySelector('.conditional__result').innerHTML = 'Надо бы повторить'
 // } else {
-//   document.querySelector(".result").innerHTML = "Ответ не ясен";
+//   document.querySelector('.conditional__result').innerHTML = 'Ответ не ясен'
 // }
+
+// let hour = 12
+// let min = 30
+// let sec = 20
+// if ((hour == 12 && min == 30) || sec == 10) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+let ne = document.querySelector('.ne-test')
+ne.addEventListener('click', function () {
+  ne.innerText = !ne.innerText
+})
+
+let input = document.querySelector('.ne-input')
+let inputValue = document.querySelector('.ne-input__value')
+
+function paste() {
+  inputValue.innerText = 'работает'
+}
+input.addEventListener('change', (event) => {
+  inputValue.innerText += input.value
+})
+let sum = 0
+while (true) {
+  let value = +prompt('Введите число', '')
+  if (!value) break
+  sum += value
+}
+console.log(sum)
